aee5a1b379354f2392a79c246306ef1e41e1e445
Darrell O'Donnell <darrell.odonnell@continuumloop.com>
2026-02-17 18:17:26 -0800

    dropped playground

--- playground/trust-registry/api/gen.sh
+++
@@ -1,2 +0,0 @@
-oapi-codegen -generate types,chi-server -o ../gen/trqp/v2/server.go -package trqp ayra-trqp-extension.yml
-oapi-codegen -generate types,chi-server -o ../gen/admin/server.go -package admin admin.yaml
